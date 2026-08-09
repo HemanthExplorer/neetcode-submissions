d641776e955218dabe4588310b9379f51c9a9d10
Hemanth M R <hemanth.mr40@gmail.com>
2026-08-09 21:17:50 +0530

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.py
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -11,10 +11,21 @@ class Solution:
          i = 0
 
          while i < len(s):
-            j = s.index('#', i)        
+            j = i 
+
+            while s[j] != "#":
+                j += 1
             length = int(s[i:j])
-            word = s[j+1 : j+1+length]
+            
+            word = s[j + 1 : j + 1 + length]
             result.append(word)
-            i = j + 1 + length 
+
+            i = j + 1 + length
 
          return result
+        
+
+
+
+
+        
