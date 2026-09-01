b17af310d413608d0e8ddcbc7a9c161e11766449
Hemanth M R <hemanth.mr40@gmail.com>
2026-09-01 21:44:27 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        se = set()
+
+        for num in nums:
+          if num in se:
+            return True
+          se.add(num)
+        return False
